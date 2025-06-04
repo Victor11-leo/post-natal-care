28da01e366409fd50319b7c9e949efb3da971a1d
lewis-developer-33 <shadowvictor33@gmail.com>
2025-06-04 08:32:03 +0300

    rm android

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname, { isCSSEnabled: true })
 
+console.log('LIGHTNINGCSS_FORCE_JS_IMPLEMENTATION:', process.env.LIGHTNINGCSS_FORCE_JS_IMPLEMENTATION);
+
+
 module.exports = withNativeWind(config, { input: './app/globals.css' })
